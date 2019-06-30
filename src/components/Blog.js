4f56308a6d79f4510a5b84ee4a94e5554935e562
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-06-30 21:36:18 +0300

    5.6* Blog list frontend, step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,49 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      {!detailsVisible ? (
+        <div>
+          <span
+            style={{ cursor: "pointer" }}
+            onClick={() => setDetailsVisible(!detailsVisible)}
+          >
+            {blog.title}
+          </span>{" "}
+          {blog.author}
+        </div>
+      ) : (
+        <div>
+          <span
+            style={{ cursor: "pointer" }}
+            onClick={() => setDetailsVisible(!detailsVisible)}
+          >
+            {blog.title}
+          </span>
+          <br />
+          <span>
+            <a href={blog.url}>{blog.url}</a>
+          </span>
+          <br />
+          <span>
+            {blog.likes} likes <button>like</button>
+          </span>
+          <br />
+          <span>added by {blog.author}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
